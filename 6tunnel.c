98f9dd9c88da65a970394cdd2d1402d51b8ca016
Wojtek Kaniewski <wojtekka@toxygen.net>
2016-10-18 21:19:52 +0200

    Handle address hints properly.

--- 6tunnel.c
+++ 6tunnel.c
@@ -1,6 +1,6 @@
 /*
- *  6tunnel v0.11
- *  (C) Copyright 2000-2005,2013 by Wojtek Kaniewski <wojtekka@toxygen.net>
+ *  6tunnel v0.12
+ *  (C) Copyright 2000-2005,2013,2016 by Wojtek Kaniewski <wojtekka@toxygen.net>
  *  
  *  Contributions by:
  *  - Dariusz Jackowski <ascent@linux.pl>
@@ -47,26 +47,27 @@ int verbose = 0, conn_count = 0;
 int remote_port, verbose, hexdump = 0;
 int remote_hint = AF_INET6;
 int local_hint = AF_INET;
-int hint_optional = 0;
 char *remote_host, *ircpass = NULL;
-char *ircsendpass = NULL, remote[128];
+char *ircsendpass = NULL;
 char *pid_file = NULL;
 const char *source_host;
 
-typedef struct map {
+typedef struct source_map {
 	char *ipv4;
 	char *ipv6;
-	struct map *next;
-} map_t;
+	struct source_map *next;
+} source_map_t;
 
-map_t *map_list = NULL;
-char *map_file = NULL;
+source_map_t *source_map = NULL;
+char *source_map_file = NULL;
 
 char *xmalloc(int size)
 {
 	char *tmp;
 
-	if (!(tmp = malloc(size))) {
+	tmp = malloc(size);
+
+	if (tmp == NULL) {
 		perror("malloc");
 		exit(1);
 	}
@@ -78,7 +79,9 @@ char *xrealloc(char *ptr, int size)
 {
 	char *tmp;
 
-	if (!(tmp = realloc(ptr, size))) {
+	tmp = realloc(ptr, size);
+
+	if (tmp == NULL) {
 		perror("realloc");
 		exit(1);
 	}
@@ -90,7 +93,9 @@ char *xstrdup(const char *str)
 {
 	char *tmp;
 
-	if (!(tmp = strdup(str))) {
+	tmp = strdup(str);
+
+	if (tmp == NULL) {
 		perror("strdup");
 		exit(1);
 	}
@@ -98,35 +103,57 @@ char *xstrdup(const char *str)
 	return tmp;
 }
 
-struct sockaddr *resolve_host(const char *name, int hint)
+char *xntop(const struct sockaddr *sa)
 {
-	struct addrinfo *ai = NULL, hints;
+	char *tmp = NULL;
+
+	if (sa->sa_family == AF_INET)
+	{
+		struct sockaddr_in *sin = (struct sockaddr_in*) sa;
+
+		tmp = xmalloc(INET_ADDRSTRLEN);
+
+		if (inet_ntop(sa->sa_family, &sin->sin_addr, tmp, INET_ADDRSTRLEN) == NULL)
+		{
+			free(tmp);
+			tmp = NULL;
+		}
+	}
+	else if (sa->sa_family == AF_INET6)
+	{		
+		struct sockaddr_in6 *sin6 = (struct sockaddr_in6*) sa;
+		tmp = xmalloc(INET6_ADDRSTRLEN);
+
+		if (inet_ntop(sa->sa_family, &sin6->sin6_addr, tmp, INET6_ADDRSTRLEN) == NULL)
+		{
+			free(tmp);
+			tmp = NULL;
+		}
+	}
+	
+	return tmp;
+}
+
+struct addrinfo *resolve_host(const char *name, int port, int hint)
+{
+	struct addrinfo *result = NULL;
+	struct addrinfo hints;
+	char port_str[16];
+	int rc;
+
+	snprintf(port_str, sizeof(port_str), "%u", port);
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = hint;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = (name == NULL && port != 0) ? AI_PASSIVE : 0;
 
-	if (getaddrinfo(name, NULL, &hints, &ai) == 0 && ai != NULL) {
-		char *tmp;
+	rc = getaddrinfo(name, (port != 0) ? port_str : NULL, &hints, &result);
 
-		tmp = xmalloc(ai->ai_addrlen);
-		memcpy(tmp, ai->ai_addr, ai->ai_addrlen);
+	if (rc == 0)
+		return result;
 
-		freeaddrinfo(ai);
-
-		return (struct sockaddr*) tmp;
-	}
-
-	/* If hints are optional, try again without them. */
-	if (hint_optional && hint != 0) {
-		struct sockaddr *res;
-
-		res = resolve_host(name, 0);
-
-		if (res != NULL) {
-			debug("warning: %s resolved ignoring hints\n", name);
-			return res;
-		}
-	}
+	debug("resolver %s:%d hint %d failed: %s\n", name, port, hint, gai_strerror(rc));
 
 	return NULL;
 }
@@ -161,33 +188,38 @@ void print_hexdump(const char *buf, int len)
 	}
 }
 
-const char *map_find(const char *ipv4)
+const char *source_map_find(const char *ipv4)
 {
-	map_t *m;
+	source_map_t *m;
 
-	for (m = map_list; m; m = m->next) {
-		if (!strcmp(m->ipv4, ipv4))
+	for (m = source_map; m != NULL; m = m->next) {
+		if (strcmp(m->ipv4, ipv4) == 0)
 			return m->ipv6;
 	}
 
-	for (m = map_list; m; m = m->next) {
-		if (!strcmp(m->ipv4, "0.0.0.0") || !strcmp(m->ipv4, "default"))
+	for (m = source_map; m != NULL; m = m->next) {
+		if ((strcmp(m->ipv4, "0.0.0.0") == 0) || (strcmp(m->ipv4, "default") == 0))
 			return m->ipv6;
 	}
 
 	return source_host;
 }
 
-void make_tunnel(int rsock, const char *remote)
+void make_tunnel(int rsock, const char *client_addr)
 {
 	char buf[4096], *outbuf = NULL, *inbuf = NULL;
 	int sock = -1, outlen = 0, inlen = 0;
 	struct sockaddr *sa = NULL;
 	const char *source;
+	struct addrinfo *connect_ai = NULL;
+	struct addrinfo *bind_ai = NULL;
+	struct addrinfo *ai_ptr;
 
-	if (map_list) {
-		if (!(source = map_find(remote))) {
-			debug("<%d> connection from unmapped address (%s), disconnecting\n", rsock, remote);
+	if (source_map != NULL) {
+		source = source_map_find(client_addr);
+
+		if (source == NULL) {
+			debug("<%d> connection from unmapped address (%s), disconnecting\n", rsock, client_addr);
 			goto cleanup;
 		}
 
@@ -195,7 +227,7 @@ void make_tunnel(int rsock, const char *remote)
 	} else
 		source = source_host;
 
-	if (ircpass) {
+	if (ircpass != NULL) {
 		int i, ret;
 
 		for (i = 0; i < sizeof(buf) - 1; i++) {
@@ -210,7 +242,7 @@ void make_tunnel(int rsock, const char *remote)
 		if (i > 0 && buf[i - 1] == '\r')
 			buf[i - 1] = 0;
 
-		if (i == 4095 || strncasecmp(buf, "PASS ", 5)) {
+		if (i == 4095 || strncasecmp(buf, "PASS ", 5) != 0) {
 			char *tmp;
 
 			debug("<%d> irc proxy auth failed - junk\n", rsock);
@@ -223,7 +255,7 @@ void make_tunnel(int rsock, const char *remote)
 			goto cleanup;
 		}
 
-		if (strcmp(buf + 5, ircpass)) {
+		if (strcmp(buf + 5, ircpass) != 0) {
 			char *tmp;
 
 			debug("<%d> irc proxy auth failed - password incorrect\n", rsock);
@@ -238,50 +270,63 @@ void make_tunnel(int rsock, const char *remote)
 		debug("<%d> irc proxy auth succeeded\n", rsock);
 	}
   
-	if (!(sa = resolve_host(remote_host, remote_hint))) {
-		debug("<%d> unable to resolve %s\n", rsock, remote_host);
+	connect_ai = resolve_host(remote_host, remote_port, remote_hint);
+
+	if (connect_ai == NULL) {
+		debug("<%d> unable to resolve %s,%d\n", rsock, remote_host, remote_port);
 		goto cleanup;
 	}
 
-	if ((sock = socket(sa->sa_family, SOCK_STREAM, 0)) == -1) {
-		debug("<%d> unable to create socket (%s)\n", rsock, strerror(errno));
-		goto cleanup;
-	}
-
-	free(sa);
-	sa = NULL;
-
-	if (source) {
-		if (!(sa = resolve_host(source, local_hint))) {
-			debug("<%d> unable to resolve source host (%s)\n", rsock, source);
+	for (ai_ptr = connect_ai; ai_ptr != NULL; ai_ptr = ai_ptr->ai_next)
+	{
+		sock = socket(ai_ptr->ai_family, ai_ptr->ai_socktype, 0);
+	
+		if (sock == -1) {
+			if (ai_ptr->ai_next != NULL)
+				continue;
+			debug("<%d> unable to create socket (%s)\n", rsock, strerror(errno));
 			goto cleanup;
 		}
 
-		if (bind(sock, sa, (local_hint == AF_INET) ? sizeof(struct sockaddr_in) : sizeof(struct sockaddr_in6))) {
-			debug("<%d> unable to bind to source host (%s)\n", rsock, source);
-			goto cleanup;
+		if (source != NULL)
+		{
+			bind_ai = resolve_host(source, 0, local_hint);
+	
+			if (bind_ai != NULL) {
+				debug("<%d> unable to resolve source host (%s)\n", rsock, (source != NULL) ? source : "default");
+				goto cleanup;
+			}
+
+			if (bind(sock, bind_ai->ai_addr, bind_ai->ai_addrlen) == -1) {
+				if (ai_ptr->ai_next != NULL) {
+					close(sock);
+					sock = -1;
+					continue;
+				}
+				debug("<%d> unable to bind to source host (%s)\n", rsock, (source != NULL) ? source : "default");
+				goto cleanup;
+			}
+
+			freeaddrinfo(bind_ai);
+			bind_ai = NULL;
 		}
 
-		free(sa);
-		sa = NULL;
+		if (connect(sock, ai_ptr->ai_addr, ai_ptr->ai_addrlen) == -1 && ai_ptr->ai_next == NULL) {
+			debug("<%d> connection failed (%s,%d)\n", rsock, remote_host, remote_port);
+			goto cleanup;
+		}
+		
+		close(sock);
+		sock = -1;
 	}
 
-	sa = resolve_host(remote_host, remote_hint);
-
-	((struct sockaddr_in*) sa)->sin_port = htons(remote_port);
-
-	if (connect(sock, sa, (sa->sa_family == AF_INET) ? sizeof(struct sockaddr_in) : sizeof(struct sockaddr_in6))) {
-		debug("<%d> connection refused (%s,%d)\n", rsock, remote_host, remote_port);
-		goto cleanup;
-	}
-
-	free(sa);
-	sa = NULL;
+	freeaddrinfo(connect_ai);
+	connect_ai = NULL;
 
 	debug("<%d> connected to %s,%d\n", rsock, remote_host, remote_port);
 
-	if (ircsendpass) {
-		snprintf(buf, 4096, "PASS %s\r\n", ircsendpass);
+	if (ircsendpass != NULL) {
+		snprintf(buf, sizeof(buf), "PASS %s\r\n", ircsendpass);
 		if (write(sock, buf, strlen(buf)) != strlen(buf))
 			goto cleanup;
 	}
@@ -335,7 +380,7 @@ void make_tunnel(int rsock, const char *remote)
 		}
 
 		if (FD_ISSET(sock, &rds)) {
-			if ((ret = read(sock, buf, 4096)) < 1)
+			if ((ret = read(sock, buf, sizeof(buf))) < 1)
 				goto cleanup;
 
 			if (hexdump) {
@@ -356,7 +401,7 @@ void make_tunnel(int rsock, const char *remote)
 		}
 
 		if (FD_ISSET(rsock, &rds)) {
-			if ((ret = read(rsock, buf, 4096)) < 1)
+			if ((ret = read(rsock, buf, sizeof(buf))) < 1)
 				goto cleanup;
 
 			if (hexdump) {
@@ -379,8 +424,11 @@ void make_tunnel(int rsock, const char *remote)
 
 
 cleanup:
-	if (sa)
-		free(sa);
+	if (connect_ai != NULL)
+		freeaddrinfo(connect_ai);
+
+	if (bind_ai != NULL)
+		freeaddrinfo(bind_ai);
 
 	close(rsock);
 
@@ -396,9 +444,9 @@ void usage(const char *arg0)
 "           [-I pass] [-L limit] [-A filename] [-p pidfile]\n"
 "           [-m mapfile] localport remotehost [remoteport]\n"
 "\n"	   
-"  -1  allow only one connection and quit\n"
-"  -4  prefer IPv4 endpoints\n"
-"  -6  bind to IPv6 address\n"
+"  -1  allow only single connection and quit\n"
+"  -4  connect to IPv4 endpoints (default: connect to IPv6)\n"
+"  -6  bind to IPv6 address (default: bind to IPv4)\n"
 "  -d  don't detach\n"
 "  -f  force tunneling (even if remotehost isn't resolvable)\n"
 "  -h  print hex dump of packets\n"
@@ -424,14 +472,14 @@ void clear_argv(char *argv)
 	return;
 }
 
-void map_destroy(void)
+void source_map_destroy(void)
 {
-	map_t *m;
+	source_map_t *m;
 	
-	debug("map_destroy()\n");
+	debug("source_map_destroy()\n");
 	
-	for (m = map_list; m; ) {
-		map_t *n;
+	for (m = source_map; m != NULL; ) {
+		source_map_t *n;
 		
 		free(m->ipv4);
 		free(m->ipv6);
@@ -440,7 +488,7 @@ void map_destroy(void)
 		free(n);
 	}
 
-	map_list = NULL;
+	source_map = NULL;
 }
 
 void map_read(void)
@@ -448,19 +496,18 @@ void map_read(void)
 	char buf[256];
 	FILE *f;
 
-	if (!map_file)
-		return;
+	debug("reading map from %s\n", source_map_file);
 
-	debug("reading map from %s\n", map_file);
+	f = fopen(source_map_file, "r");
 
-	if (!(f = fopen(map_file, "r"))) {
+	if (f == NULL) {
 		debug("unable to read map file, ignoring\n");
 		return;
 	}
 	
-	while (fgets(buf, sizeof(buf), f)) {
+	while (fgets(buf, sizeof(buf), f) != NULL) {
 		char *p, *ipv4, *ipv6;
-		map_t *m;
+		source_map_t *m;
 
 		for (p = buf; *p == ' ' || *p == '\t'; p++);
 
@@ -490,11 +537,11 @@ void map_read(void)
 
 		debug("[%s] mapped to [%s]\n", ipv4, ipv6);
 		
-		m = (map_t*) xmalloc(sizeof(map_t));
+		m = (source_map_t*) xmalloc(sizeof(source_map_t));
 		m->ipv4 = xstrdup(ipv4);
 		m->ipv6 = xstrdup(ipv6);
-		m->next = map_list;
-		map_list = m;
+		m->next = source_map;
+		source_map = m;
 	}
 	
 	fclose(f);
@@ -502,7 +549,7 @@ void map_read(void)
 
 void sighup()
 {
-	map_destroy();
+	source_map_destroy();
 	map_read();
 
 	signal(SIGHUP, sighup);
@@ -520,7 +567,7 @@ void sigchld()
 
 void sigterm()
 {
-	if (pid_file)
+	if (pid_file != NULL)
 		unlink(pid_file);
 
 	exit(0);
@@ -528,19 +575,20 @@ void sigterm()
 
 int main(int argc, char **argv)
 {
-	int force = 0, lsock, csock, one = 0, jeden = 1, local_port;
+	int force = 0, listen_fd, single_connection = 0, jeden = 1, local_port;
 	int detach = 1, sa_len, conn_limit = 0, optc;
-	char *username = NULL, *bind_host = NULL;
+	char *username = NULL, *local_host = NULL;
+	struct addrinfo *ai;
+	struct addrinfo *ai_ptr;
 	struct sockaddr *sa;
-	struct sockaddr_in laddr, caddr;
+	struct sockaddr_in laddr;
 	struct sockaddr_in6 laddr6;
-	unsigned int caddrlen = sizeof(caddr);
 	struct passwd *pw = NULL;
 	
 	while ((optc = getopt(argc, argv, "1dv46fHs:l:I:i:hu:m:L:A:p:")) != -1) {
 		switch (optc) {
 			case '1':
-				one = 1;
+				single_connection = 1;
 				break;
 			case 'd':
 				detach = 0;
@@ -549,16 +597,16 @@ int main(int argc, char **argv)
 				verbose = 1;
 				break;
 			case '4':
+				remote_hint = AF_INET;
 				break;
 			case '6':
-				remote_hint = AF_INET;
 				local_hint = AF_INET6;
 				break;
 			case 's':
 				source_host = xstrdup(optarg);
 				break;
 			case 'l':
-				bind_host = xstrdup(optarg);
+				local_host = xstrdup(optarg);
 				break;
 			case 'f':
 				force = 1;
@@ -578,7 +626,7 @@ int main(int argc, char **argv)
 				username = xstrdup(optarg);
 				break;
 			case 'm':
-				map_file = xstrdup(optarg);
+				source_map_file = xstrdup(optarg);
 				break;
 			case 'L':
 				conn_limit = atoi(optarg);
@@ -587,7 +635,7 @@ int main(int argc, char **argv)
 				pid_file = xstrdup(optarg);
 				break;
 			case 'H':
-				hint_optional = 1;
+				fprintf(stderr, "Warning: Option -H is deprecated\n");
 				break;
 			default:
 				return 1;
@@ -608,87 +656,85 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	if (username && !(pw = getpwnam(username))) {
-		fprintf(stderr, "%s: unknown user %s\n", argv[0], username);
-		exit(1);
+	if (username != NULL) {
+		pw = getpwnam(username);
+
+		if (pw == NULL) {
+			fprintf(stderr, "%s: unknown user %s\n", argv[0], username);
+			exit(1);
+		}
 	}
   
-	if (map_file)
+	if (source_map_file != NULL)
 		map_read();
   
 	local_port = atoi(argv[optind++]);
 	remote_host = argv[optind++];
 	remote_port = (argc == optind) ? local_port : atoi(argv[optind]);
 
+	/* Check if destination host is resolvable. If it's expected to be available later,
+	 * -f can be used. */
+ 
 	debug("resolving %s\n", remote_host);
 
-	if (!(sa = resolve_host(remote_host, remote_hint)) && !force) {
+	ai = resolve_host(remote_host, remote_port, remote_hint);
+
+	if (ai == NULL && !force) {
 		fprintf(stderr, "%s: unable to resolve host %s\n", argv[0], remote_host);
 		exit(1);
 	}
 
-	free(sa);
-	sa = NULL;
+	freeaddrinfo(ai);
 
-	if (bind_host) {
-		debug("resolving %s\n", bind_host);
+	/* Resolve local address for bind(). In case of NULL address resolve_host() will
+	 * return INADDR_ANY or in6addr_any, so we can bind either way. */
 
-		if (!(sa = resolve_host(bind_host, local_hint))) {
-			fprintf(stderr, "%s: unable to resolve host %s\n", argv[0], remote_host);
-			exit(1);
-		}
+	debug("resolving local address %s\n", (local_host != NULL) ? local_host : "default");
+
+	ai = resolve_host(local_host, local_port, local_hint);
+
+	if (ai == NULL) {
+		fprintf(stderr, "%s: unable to resolve host %s\n", argv[0], local_host);
+		exit(1);
 	}
+
+	/* Now that we know that hosts are resolvable, dump some debugging information. */
  
-	debug("local: %s,%d; ", (bind_host) ? bind_host : "default", local_port);
+	debug("local: %s,%d; ", (local_host != NULL) ? local_host : "default", local_port);
 	debug("remote: %s,%d; ", remote_host, remote_port);
 
-	if (map_file)
+	if (source_map_file != NULL)
 		debug("source: mapped\n");
 	else
-		debug("source: %s\n", (source_host) ? source_host : "default");
+		debug("source: %s\n", (source_host != NULL) ? source_host : "default");
 
-	if (local_hint == AF_INET) {
-		lsock = socket(PF_INET, SOCK_STREAM, 0);
+	/* Now bind. */
 
-		memset(&laddr, 0, (sa_len = sizeof(laddr)));
-		laddr.sin_family = AF_INET;
-		laddr.sin_port = htons(local_port);
-		
-		if (sa) {
-			memcpy(&laddr.sin_addr, &((struct sockaddr_in*) sa)->sin_addr, sizeof(struct in_addr));
-			free(sa);
-		}
-		
-		sa = (struct sockaddr*) &laddr;
-	} else {
-		lsock = socket(PF_INET6, SOCK_STREAM, 0);
-		
-		memset(&laddr6, 0, (sa_len = sizeof(laddr6)));
-		laddr6.sin6_family = AF_INET6;
-		laddr6.sin6_port = htons(local_port);
-		
-		if (sa) {
-			memcpy(&laddr6.sin6_addr, &((struct sockaddr_in6*) sa)->sin6_addr, sizeof(struct in6_addr));
-			free(sa);
-		}
+	listen_fd = socket(ai->ai_family, ai->ai_socktype, 0);
 
-		sa = (struct sockaddr*) &laddr6;
+	if (listen_fd == -1) {
+		perror("socket");
+		exit(1);
 	}
 
-	if (setsockopt(lsock, SOL_SOCKET, SO_REUSEADDR, &jeden, sizeof(jeden)) == -1) {
+	if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &jeden, sizeof(jeden)) == -1) {
 		perror("setsockopt");
 		exit(1);
 	}
   
-	if (bind(lsock, sa, sa_len)) {
-		perror("bind");
-		exit(1);
+	for (ai_ptr = ai; ai_ptr != NULL; ai_ptr = ai_ptr->ai_next) {
+		if (bind(listen_fd, ai_ptr->ai_addr, ai_ptr->ai_addrlen) == -1 && ai_ptr->ai_next == NULL) {
+			perror("bind");
+			exit(1);
+		}
 	}    
   
-	if (listen(lsock, 100)) {
+	if (listen(listen_fd, 100) == -1) {
 		perror("listen");
 		exit(1);
 	}
+
+	/* Daemonize. */
 
 	if (detach) {
 		int i, ret;
@@ -709,7 +755,9 @@ int main(int argc, char **argv)
 			exit(0);
 	}
 
-	if (pid_file) {
+	/* Store process id if requested. */
+
+	if (pid_file != NULL) {
 		FILE *f = fopen(pid_file, "w");
 
 		if (!f)
@@ -720,9 +768,13 @@ int main(int argc, char **argv)
 		}
 	}
 
-	if (username && ((setgid(pw->pw_gid) == -1) || (setuid(pw->pw_uid) == -1))) {
-		perror("setuid/setgid");
-		exit(1);
+	/* Change user and group id if requested. */
+
+	if (pw != NULL) {
+		if ((setgid(pw->pw_gid) == -1) || (setuid(pw->pw_uid) == -1)) {
+			perror("setuid/setgid");
+			exit(1);
+		}
 	}
 
 	setsid();
@@ -734,11 +786,16 @@ int main(int argc, char **argv)
 	for (;;) {  
 		int ret;
 		fd_set rds;
+		int client_fd;
+		char *client_addr;
+		int client_port;
+		struct sockaddr sa;
+		unsigned int sa_len = sizeof(sa);
 
 		FD_ZERO(&rds);
-		FD_SET(lsock, &rds);
+		FD_SET(listen_fd, &rds);
 
-		if (select(lsock + 1, &rds, NULL, NULL, NULL) == -1) {
+		if (select(listen_fd + 1, &rds, NULL, NULL, NULL) == -1) {
 			if (errno == EINTR)
 				continue;
 
@@ -746,22 +803,24 @@ int main(int argc, char **argv)
 			break;
 		}
 
-		if ((csock = accept(lsock, (struct sockaddr*) &caddr, &caddrlen)) == -1) {
+		client_fd = accept(listen_fd, &sa, &sa_len);
+		
+		if (client_fd == -1) {
 			perror("accept");
 			break;
 		}
 
-		inet_ntop(caddr.sin_family, (caddr.sin_family == AF_INET) ?
-			&caddr.sin_addr :
-			(void*) &(((struct sockaddr_in6*)&caddr)->sin6_addr),
-			remote, sizeof(remote));
+		client_addr = xntop(&sa);
 
-		debug("<%d> connection from %s,%d", csock, remote, ntohs(caddr.sin_port));
+		client_port = (sa.sa_family == AF_INET) ? ((struct sockaddr_in*) &sa)->sin_port :
+			((struct sockaddr_in6*) &sa)->sin6_port;
+
+		debug("<%d> connection from %s,%d", client_fd, client_addr, ntohs(client_port));
 
 		if (conn_limit && (conn_count >= conn_limit)) {
 			debug(" -- rejected due to limit.\n");
-			shutdown(csock, 2);
-			close(csock);
+			shutdown(client_fd, 2);
+			close(client_fd);
 			continue;
 		}
 		
@@ -774,31 +833,34 @@ int main(int argc, char **argv)
     
 		if ((ret = fork()) == -1) {
 			debug(" -- fork() failed.\n");
-			shutdown(csock, 2);
-			close(csock);
+			shutdown(client_fd, 2);
+			close(client_fd);
+			free(client_addr);
 			continue;
 		}
     
 		if (!ret) {
 			signal(SIGHUP, SIG_IGN);
-			close(lsock);
+			close(listen_fd);
 			debug("\n");
-			make_tunnel(csock, remote);
-			debug("<%d> connection closed\n", csock);
+			make_tunnel(client_fd, client_addr);
+			free(client_addr);
+			debug("<%d> connection closed\n", client_fd);
 			exit(0);
 		} 
 
-		close(csock);
+		close(client_fd);
+		free(client_addr);
     
-		if (one) {
-			shutdown(lsock, 2);
-			close(lsock);
+		if (single_connection) {
+			shutdown(listen_fd, 2);
+			close(listen_fd);
 			exit(0);
 		}
 
 	}
 
-	close(lsock);
+	close(listen_fd);
   
 	exit(1);
 }
