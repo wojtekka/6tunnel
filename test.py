181adf9485b5f2e4afa79f39ee0dace6bce86d84
Wojtek Kaniewski <wojtekka@toxygen.net>
2016-10-23 23:10:11 +0200

    Add some more tests.

--- test.py
+++ test.py
@@ -5,10 +5,12 @@ import socket
 import time
 import select
 
-(SUCCESS, COMMAND_FAILED, CONNECT_FAILED, DISCONNECT, ACCEPT_FAILED, DATA_FAILED) = range(6)
+(SUCCESS, COMMAND_FAIL, CONNECT_FAIL, DISCONNECT, ACCEPT_FAIL, DATA_MISMATCH) = range(6)
+labels = ["success", "command fail", "connection fail", "disconnection", "accept fail", "data mismatch"]
 
-def test(expect, client_af, server_af, from_ip, to_ip, args=""):
+def test(expect, client_af, server_af, from_ip, to_ip, args="", client_sends_first="NICK nick\r\n", server_receives="NICK nick\r\n", app_responds="", app_inserts="", server_sends_then=":localhost 001 nick :Welcome\r\n"):
     # Open and close a socket to get random port available
+
     client_sock = socket.socket(client_af, socket.SOCK_STREAM, 0)
     client_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
     client_sock.bind(('', 0))
@@ -16,31 +18,60 @@ def test(expect, client_af, server_af, from_ip, to_ip, args=""):
     client_sock.close()
 
     # Open a socket for mock server
+
     server_sock = socket.socket(server_af, socket.SOCK_STREAM, 0)
     server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
     server_sock.bind(('', 0))
     server_sock.listen(0)
     server_port = server_sock.getsockname()[1]
 
-    if os.system("./6tunnel -1 %s %d %s %d" % (args, client_port, to_ip, server_port)) != 0:
-        if expect != COMMAND_FAILED:
-            raise Exception("expected % yet command failed" % expect)
+    all_args = "-1 %s %d %s %d" % (args, client_port, to_ip, server_port)
+    print "Running with %s" % all_args
+    if os.system("./6tunnel " + all_args) != 0:
+        if expect != COMMAND_FAIL:
+            raise Exception("expected %s yet command failed" % labels[expect])
         else:
             return
 
     client_sock = socket.socket(client_af, socket.SOCK_STREAM, 0)
 
     # Give 6tunnel instance some time to initialize
+
     connected = False
     for i in range(10):
         try:
             client_sock.connect((from_ip, client_port))
         except socket.error:
             time.sleep(0.1)
-        connected = True
-        break
+        else:
+            connected = True
+            break
 
-    rlist, wlist, xlist = select.select([client_sock], [], [client_sock], 1)
+    if not connected:
+        if expect != CONNECT_FAIL:
+            raise Exception("expected %s yet connect failed" % labels[expect])
+        else:
+            return
+
+    # Send first data
+
+    client_sock.send(client_sends_first)
+
+    # Wait for 6tunnel to connect to the server
+
+    rlist, wlist, xlist = select.select([server_sock], [], [], 1)
+
+    if not rlist:
+        if expect != ACCEPT_FAIL:
+            raise Exception("expected %s yet accept failed" % labels[expect])
+        else:
+            return
+
+    accept_sock = server_sock.accept()[0]
+
+    # Make sure that 6tunnel doesn't send anything to the client
+
+    rlist, wlist, xlist = select.select([client_sock], [], [], 1)
 
     if rlist:
         try:
@@ -49,42 +80,31 @@ def test(expect, client_af, server_af, from_ip, to_ip, args=""):
                 raise socket.error
         except socket.error:
             if expect != DISCONNECT:
-                raise Exception("expected %d yet disconnected" % expect)
+                raise Exception("expected %s yet disconnected" % labels[expect])
             else:
                 return
 
         raise Exception("unexpected data sent to client")
 
-    if not connected:
-        if expect != CONNECT_FAILED:
-            raise Exception("expected %d yet connect failed" % expect)
-        else:
-            return
+    # Do the data exchange
 
-    rlist, wlist, xlist = select.select([server_sock], [], [], 1)
+    if app_responds:
+        tmp = client_sock.recv(len(app_responds))
+        if tmp != app_responds:
+            raise Exception("expected 6tunnel response \"%s\" yet did not receive" % app_responds)
 
-    if not rlist:
-        if expect != ACCEPT_FAILED:
-            raise Exception("expected %d yet accept failed" % expect)
-        else:
-            return
+    if app_inserts:
+        tmp = accept_sock.recv(len(app_inserts))
+        if tmp != app_inserts:
+            raise Exception("expected 6tunnel insert \"%s\" yet did not receive" % app_sends_first)
 
-    accept_sock = server_sock.accept()[0]
-    accept_sock.send("ABC")
+    if accept_sock.recv(len(server_receives)) != server_receives:
+        raise Exception("data mismatch")
 
-    if client_sock.recv(3) != "ABC":
-        if expect != DATA_FAILED:
-            raise Exception("expected %d yet data failed" % expect)
-        else:
-            return
+    accept_sock.send(server_sends_then)
 
-    client_sock.send("DEF")
-
-    if accept_sock.recv(3) != "DEF":
-        if expect != DATA_FAILED:
-            raise Exception("expected %d yet data failed" % expect)
-        else:
-            return
+    if client_sock.recv(len(server_sends_then)) != server_sends_then:
+        raise Exception("data mismatch")
 
     accept_sock.close()
     server_sock.close()
@@ -95,25 +115,35 @@ def test(expect, client_af, server_af, from_ip, to_ip, args=""):
 
 test(SUCCESS, socket.AF_INET, socket.AF_INET6, '127.0.0.1', '::1')
 test(SUCCESS, socket.AF_INET, socket.AF_INET6, '127.0.0.1', '::1', '-l 127.0.0.1')
-test(COMMAND_FAILED, socket.AF_INET, socket.AF_INET6, '127.0.0.1', '::1', '-l ::1')
+test(COMMAND_FAIL, socket.AF_INET, socket.AF_INET6, '127.0.0.1', '::1', '-l ::1')
 test(SUCCESS, socket.AF_INET, socket.AF_INET6, '127.0.0.1', '::1', '-s ::1')
-test(COMMAND_FAILED, socket.AF_INET, socket.AF_INET6, '127.0.0.1', '::1', '-s 127.0.0.1')
+test(COMMAND_FAIL, socket.AF_INET, socket.AF_INET6, '127.0.0.1', '::1', '-s 127.0.0.1')
 
 test(SUCCESS, socket.AF_INET, socket.AF_INET, '127.0.0.1', '127.0.0.1', '-4')
 test(SUCCESS, socket.AF_INET, socket.AF_INET, '127.0.0.1', '127.0.0.1', '-4 -l 127.0.0.1')
-test(COMMAND_FAILED, socket.AF_INET, socket.AF_INET, '127.0.0.1', '127.0.0.1', '-4 -l ::1')
+test(COMMAND_FAIL, socket.AF_INET, socket.AF_INET, '127.0.0.1', '127.0.0.1', '-4 -l ::1')
 test(SUCCESS, socket.AF_INET, socket.AF_INET, '127.0.0.1', '127.0.0.1', '-4 -s 127.0.0.1')
-test(COMMAND_FAILED, socket.AF_INET, socket.AF_INET, '127.0.0.1', '127.0.0.1', '-4 -s ::1')
+test(COMMAND_FAIL, socket.AF_INET, socket.AF_INET, '127.0.0.1', '127.0.0.1', '-4 -s ::1')
 
 test(SUCCESS, socket.AF_INET6, socket.AF_INET, '::1', '127.0.0.1', '-4 -6')
 test(SUCCESS, socket.AF_INET6, socket.AF_INET, '::1', '127.0.0.1', '-4 -6 -l ::1')
-test(COMMAND_FAILED, socket.AF_INET6, socket.AF_INET, '::1', '127.0.0.1', '-4 -6 -l 127.0.0.1')
+test(COMMAND_FAIL, socket.AF_INET6, socket.AF_INET, '::1', '127.0.0.1', '-4 -6 -l 127.0.0.1')
 test(SUCCESS, socket.AF_INET6, socket.AF_INET, '::1', '127.0.0.1', '-4 -6 -s 127.0.0.1')
-test(COMMAND_FAILED, socket.AF_INET6, socket.AF_INET, '::1', '127.0.0.1', '-4 -6 -s ::1')
+test(COMMAND_FAIL, socket.AF_INET6, socket.AF_INET, '::1', '127.0.0.1', '-4 -6 -s ::1')
 
 test(SUCCESS, socket.AF_INET6, socket.AF_INET6, '::1', '::1', '-6')
 test(SUCCESS, socket.AF_INET6, socket.AF_INET6, '::1', '::1', '-6 -l ::1')
-test(COMMAND_FAILED, socket.AF_INET6, socket.AF_INET6, '::1', '::1', '-6 -l 127.0.0.1')
+test(COMMAND_FAIL, socket.AF_INET6, socket.AF_INET6, '::1', '::1', '-6 -l 127.0.0.1')
 test(SUCCESS, socket.AF_INET6, socket.AF_INET6, '::1', '::1', '-6 -s ::1')
-test(COMMAND_FAILED, socket.AF_INET6, socket.AF_INET6, '::1', '::1', '-6 -s 127.0.0.1')
+test(COMMAND_FAIL, socket.AF_INET6, socket.AF_INET6, '::1', '::1', '-6 -s 127.0.0.1')
+
+# Test IRC password options
+
+test(SUCCESS, socket.AF_INET, socket.AF_INET6, '127.0.0.1', '::1', '-I password', app_inserts="PASS password\r\n")
+
+test(ACCEPT_FAIL, socket.AF_INET, socket.AF_INET6, '127.0.0.1', '::1', '-i password', client_sends_first="NICK nick\r\n")
+
+test(ACCEPT_FAIL, socket.AF_INET, socket.AF_INET6, '127.0.0.1', '::1', '-i password', client_sends_first="PASS invalid\r\nNICK nick\r\n", app_responds=":6tunnel 464 * :Password incorrect\r\n")
+
+test(SUCCESS, socket.AF_INET, socket.AF_INET6, '127.0.0.1', '::1', '-i password', client_sends_first="PASS password\r\nNICK nick\r\n")
 
